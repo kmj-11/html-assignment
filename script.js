7ae518c7588eea05b5d44582144f9940395347a5
Kayla Jacobs <kayla.jacobs98@gmail.com>
2025-05-27 21:50:15 +0200

    added javascript function to show a welcome message on a button click.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // script.js
+
+// function showGreeting() {
+const message = document.getElementById("message");
+message.textContent = "Welcome to my Hobby Portfolio";
+message.style.display = "block";
+}
+
+// Add a click event to the button
+document.addEventListener("DOMContentLoaded", function () {
+const button = document.getElementById("greetButton");
+button.addEventListener("click", showGreeting);
+}

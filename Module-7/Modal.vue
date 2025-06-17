b2b8f2e7671e2e2a27d944da708fd75faeee3d36
Kayla Jacobs <kayla.jacobs98@gmail.com>
2025-06-17 22:22:50 +0200

    added re-usable modal component with props and close event

---
+++ Module-7/Modal.vue
@@ -0,0 +1,16 @@
+export default {
+  name: 'Modal',
+  props: ['name', 'email', 'message'],
+  emits: ['close'],
+  template: `
+    <div class="modal">
+      <div class="modal-content">
+        <span class="close" @click="$emit('close')">&times;</span>
+        <h2>Thank you!</h2>
+        <p><strong>Name:</strong> {{ name }}</p>
+        <p><strong>Email:</strong> {{ email }}</p>
+        <p><strong>Message:</strong> {{ message }}</p>
+      </div>
+    </div>
+  `
+}

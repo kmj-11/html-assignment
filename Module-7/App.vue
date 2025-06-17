5329f21a7f12fa0c5202284021d1b2b460e4528a
Kayla Jacobs <kayla.jacobs98@gmail.com>
2025-06-17 21:56:11 +0200

    set up Vue app and register ContactForm component

---
+++ Module-7/App.vue
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+import ContactForm from './ContactForm.vue'
+createApp({
+    components: {
+        ContactForm
+    },
+    template: `
+        <main>
+            <h1>Contact Me</h1>
+            <ContactForm
+                namePlaceholder="Enter your name"
+                emailPlaceholder="ENter your email"
+                messagePlaceholder="Write your message"
+                submitLabel="Send"
+            />
+        </main>
+    `
+}).mount('#app')
